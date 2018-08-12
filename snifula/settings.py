78c01c3a4f90e4ea089d6bb3bcbaef6b567810a9
Jyry Hjelt <hjelt.jyry@gmail.com>
2018-08-12 21:37:16 +0200

    removed secret key and replaced by an environment variable

--- snifula/settings.py
+++ snifula/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'na8him)8y2_5fdwnex8ou@o=y8m*(@wh9krwip1!9kv(9_xi3w'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
