6c2ea65390f3959a000d7663ffaa5529dab670e8
Jyry Hjelt <hjelt.jyry@gmail.com>
2018-08-13 23:13:19 +0200

    updated staticfiles settings

--- snifula/settings.py
+++ snifula/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 
 AUTH_USER_MODEL = 'users.Account'
--- snifula/urls.py
+++ snifula/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -27,6 +28,4 @@ urlpatterns = [
     path('about/', views.aboutView, name='about'),
     path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
