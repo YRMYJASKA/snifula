6ba4347487a2d6d6cb05ec5a6abe36d63f5e40bd
Jyry Hjelt <hjelt.jyry@gmail.com>
2018-08-14 00:09:55 +0200

    removed captcha from forum forms

--- forum/forms.py
+++ forum/forms.py
@@ -1,24 +1,21 @@
 from django import forms
-from captcha.fields import CaptchaField
 
 from . import models
 
 
 class AddUserPostForm(forms.ModelForm):
     """Form to add new UserPosts"""
-    captcha = CaptchaField()
     class Meta:
         model = models.UserPost
         fields = ('title', 'content')
         labels = {
-                'title': 'Title',
-                'content': '',
-                }
+            'title': 'Title',
+            'content': '',
+        }
 
 
 class UserCommentForm(forms.ModelForm):
 
-    captcha = CaptchaField()
     class Meta:
         model = models.UserComment
         fields = ('content', )
