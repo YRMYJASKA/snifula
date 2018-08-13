bbe3353c116798f13bd9803033e9e60ad9686fdc
Jyry Hjelt <hjelt.jyry@gmail.com>
2018-08-13 23:24:40 +0200

    yet another fix..

--- snifula/settings.py
+++ snifula/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'snifula.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'snifula/templates'), os.path.join(BASE_DIR, 'users/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
